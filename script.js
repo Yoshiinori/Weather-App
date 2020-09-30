850499294ec33a2a210fc59a9c902b16895efe4e
Yoshiinori <yoshiinoridev@gmail.com>
2020-09-30 06:44:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = '7f90da554b9e38ab6f360f884e8542e3'
+
+const city = document.querySelector('.city')
+const ctemp = document.querySelector('.ctemp')
+const ftemp = document.querySelector('.ftemp')
+
+const getLocation = (position) => {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      city.innerHTML = data.name;
+      ctemp.innerHTML = data.main.temp;
+      ftemp.innerHTML = data.main.temp * 9 / 5 + 32;
+    });
+}
+
+const noLocation = (err) => {
+  console.error(err)
+}
+
+navigator.geolocation.getCurrentPosition(getLocation, noLocation)
